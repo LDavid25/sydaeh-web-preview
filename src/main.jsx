a3854386034e007047e1c5c4469db64ba32d1e48
LDavid <ldavid2514@gmail.com>
2024-10-23 13:42:27 -0400

    Ensure inicio.jsx is tracked

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Header } from './header';
-import { Nosotros } from './nosotros';
-import { Servicios } from './servicios';
-import { Productos } from './productos';
-import { Inicio } from '/src/inicio';
-import { Footer } from './footer';
+import  Header  from './header';
+import  Nosotros  from './nosotros';
+import  Servicios  from './servicios';
+import  Productos  from './productos';
+import  Inicio  from './inicio';
+import  Footer  from './footer';
 import './index.css';
 
 
--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/"
-})
+  resolve: {
+    alias: {
+      '@': '/src', // Alias para simplificar importaciones
+    },
+  },
+  build: {
+    rollupOptions: {
+      external: [], // Asegúrate de que el módulo no esté marcado como externo
+    },
+  },
+});
