05424bd3747444ce4ac9ba661b728f7e30660745
LDavid <ldavid2514@gmail.com>
2024-10-23 15:36:21 -0400

    debug rename inicio/home

--- src/Home/Index.jsx
+++ src/Home/Index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 
-const Inicio = () => {
+const Home = () => {
 
     return(
         <>
@@ -11,4 +11,4 @@ const Inicio = () => {
     
 }
 
-export default Inicio;
+export default Home;
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import  Header  from './header';
-import  Inicio  from './inicio/index.jsx';
+import  Home  from './home';
 import  Nosotros  from './nosotros';
 import  Servicios  from './servicios';
 import  Productos  from './productos';
@@ -17,7 +17,7 @@ createRoot(document.getElementById('root')).render(
         <Route path="/nosotros" element={<Nosotros />} />
         <Route path="/servicios" element={<Servicios />} />
         <Route path="/productos" element={<Productos />} />
-        {<Route path="/" element={<Inicio />} />}
+        <Route path="/" element={<Home />} />
       </Routes>
       <Footer />
     </Router>
