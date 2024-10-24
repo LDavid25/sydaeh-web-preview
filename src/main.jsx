ed658124784a4da4de322fa324e51d380df7cf91
LDavid <ldavid2514@gmail.com>
2024-10-23 21:01:59 -0400

    debugin could not resolve home

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import  Header  from './header';
-import  Home  from './home';
+import  Home  from './Home';
 import  Nosotros  from './nosotros';
 import  Servicios  from './servicios';
 import  Productos  from './productos';
@@ -14,10 +14,10 @@ createRoot(document.getElementById('root')).render(
     <Router>
       <Header />
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/nosotros" element={<Nosotros />} />
         <Route path="/servicios" element={<Servicios />} />
         <Route path="/productos" element={<Productos />} />
-        <Route path="/" element={<Home />} />
       </Routes>
       <Footer />
     </Router>
